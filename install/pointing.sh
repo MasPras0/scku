68215a4e782119732ab91f0f72307e55b7d017ff
MasPras0 <prasetyodevv@gmail.com>
2025-01-28 21:39:48 +0700

    Update pointing.sh

--- install/pointing.sh
+++ install/pointing.sh
@@ -1,4 +1,79 @@
 #!/bin/bash
+apt install jq curl -y
+if [[ ! -e /etc/github/cf ]]; then
+mkdir -p /etc/github
+curl -s https://pastebin.com/raw/Gw35j9qh > /etc/github/cf
+fi
+DO=$(cat /etc/xray/domain | cut -d "." -f2-)
+SUB=$(cat /etc/xray/domain | cut -d "." -f1)
 
-God="BlessYou";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=TF;oTX=dD;Yox=Qa;wQx=mb;Kqb=Zy;xWU=twB;gTp=auw;hwZ=Ejp;tHg=iQZ;ojo=shb;jhw=FjF;IDL=klV;wOA=yFT;PZL=XfL;DRJ=DiE;bmJ=FCa;XGN=dOT;QCK=dkY;SOb=WJE;RJx=uAv;czJ=dts;VGB=oCy;TyO=xGj;yHU=bpq;Nfb=Wct;doQ=Ibf;XCL=rax;ZNH=Hhf;Eoh=yCU;ghd=hut;ooz=VKY;qMf=PrY;Ecf=Lke;BbJ=jie;QOI=lOT;oi=QkU;yM=iDO;rT=twI;wa=BEk;Lf=SGl;Sx=Kri;yL=RiF;Xf=iZT;iv=fpC;fW=nki;qX=FrL;ba=OUY;czP=Hdm;ecN=WKt;QMW=BnB;Nsq=jdC;nof=OZt;bhx=qNV;vVN=xaU;smp=Wta;ceY=GDh;AIB=juO;miA=Cso;zgz=Hka;Kea=YMq;hUp=SWY;zMP=ozk;WGK=JEt;pkF=jjF;FCq=pFs;ZrQ=VcQ;ZjS=do;BTP=Su;sly=xM;aZl=vF;iJn=bo;QCl=HB;SCO=xo;fUq=Jo;Epq=zc;wxq=Ev;ziJ=Gx;Kzo=JF;PDO=pJ;qhq=yp;OYk=rj;OGy=Cc;hSK=xR;lnD=um;UXi=pl;xBF=xP;qOD=iX;RVy=nl;mqe=JK;rQt=CA;qJD=FR;sPp=XZ;moh=DC;Fsj=GWh;NTx=Jnx;dwG=ncr;Ugd=mFV;ZTt=WjI;mFG=lms;YGK=bPI;UKs=ibv;hka=JFE;zpQ=Gxt;VlF=Jxk;AEJ=cuk;SUZ=YiV;Wfq=NhZ;xLp=ray;QmE=mqM;fkp=tHl;Sek=XvT;qbj=EKu;RfA=xSe;fPQ=Oll;cif=jtg;GYE=yei;Prw=RTD;sMB=pEL;sxA=APb;aRs=HgZ;SXm=VSI;lZA=UKc;YMw=Lct;bwO=XQI;OxQ=COX;bPh=KqP;GVr=hRx;eoA=eID;AZK=fFj;PGC=XPy;ieV=gly;AUO=yit;GjW=jaZ;tBB=HSO;vdV=efl;HiW=VHx;JaQ=RCo;cGr=fsk;eib=jty;TPK=rlb;dhM=oDt;tCL=muK;gAg=xQq;xWl=eMt;AtB=mbQ;UzS=lee;xjU=CPZ;qLS=iIg;dQw=NHN;Dum=qAc;aei=Whw;afz=OcR;NWZ=Txs;gzI=HsZ;YmH=IIm;kZc=Ppd;qnZ=vjw;sQw=BmF;PAB=rGe;gah=aHu;XfD=uJJ;LmJ=PUK;jCz=XpN;gTg=hRF;cpS=fhx;wFI=fih;pHM=aTH;GIm=Jtb;XDy=XGY;zMs=kPd;VtP=pbP;ALj=acU;HEZ=Tre;EPr=vOf;eSC=AIk;TXH=szR;nNa=bGD;lQt=Qod;Qvm=HGI;tkS=IOY;qmZ=tDf;xxx=xOS;pFg=XCd;aVR=xGh;ooR=AHX;JVj=laY;TvM=kEx;SYU=hol;ddj=khS;aPP=oiI;Edo="
-";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt="l";Yxgs="";qwt="Fu";ieC=HO;XcH=Rv;Ugs=LC;vD=xp;fy=oW;Zl=LhH;oP=Jm;sDA=kpY;GHn=TB;wzk=Da;oiO=Ei;NaM=hIf;fC=vjz;kT=rz;sV=bZz;CL=ARL;Wc=dA;gZG=tGI;vpk=cM;Aao=tgy;sU=ld;LZ=Pr;BE=wCX;iN=fQ;RCe=Dm;Rc=Fl;Njo=OMq;gtb=SKa;Wf=jMB;Dm=wHr;Nx=Wn;ANT=jAK;oFy=Bnc;IjZ=Xj;DQa=oK;ewZ=Vm;QPf=lz;Cwg=aS;KEM=jS;fkE=hM;nLw=ym;FWv=BC;aDC=Lw;tW=Rgn;kw=Hgf;tQ=eSh;Fi=iGb;Or=snM;ZG=xqx;xC=WJr;Bf=haC;pT=THr;By=jbu;tm=Hcy;eT=nTC;UT=KCa;dk=Pxy;vG=TvX;HX=MMB;Ow=ZoT;UJ=LvE;rx=oMF;EB=VZx;OC=IfU;dv=lTx;Nk=TNq;Ej=bKu;dl=ZZi;yzL=bJ;EiG=LJ;iqg=gK;fbd=vg;OPl=Ei;tnI=YS;yBV=DA;yxh=Ks;Nic=Ih;wMq=tD;iDs=AQ;Nhh=zy;JWe=Ry;gC=AnI;qWN=uq;hIc=OAL;UOm=Nq;Xj=ax;GW=utn;Fmc=CaI;zeK=eT;hI=sl;Ae=RK;XV=doy;Dpq=Eg;Mmr=MZg;CXd=Bao;RND=WUC;CGm=SRK;DJh=LZw;ym=Ah;xzv=IX;FYi=YUX;v6T=" ";RzE="iVHa0l2ZvMGdl9CIl1CIhAyWbBiZppQetACbyV3YgEnagwGbhR3culGI0BXYKg2chJ2LulmYvEyI pJWZ0NXYw9yL6MHc0RHagMXLgwmc1NmCiVHa0l2ZvMGdl9CIw1CIylGZr1mCuVGa0ByOd1FImN2L 5Fmc49yY0V2LgQXYjhCJ98ERKkmZKY2YvIWdoRXan9yY0V2Lg4DITd3ZXBFVRZ2L3Fmcv02bj5ib jBCfg4Wah12bk9SehJHevMGdl9CI0F2YoQSPCV1UKkSLyYWLgIiLiACZtACd1NGI8BibpFWbvR2L n52bz92agE2appGIscmbvN3brByTEBCahtWYwFGIhN3apJXZtVWTgMiCKkSMm1CIi4iIgQWLgQXd gAiCuVGa0ByOd1FIi8ERkICI61CIbtFImlmCnUmcvR3cu42bsFGcnAibhdmblRGIpNXagE2ah1GI rlmagwyZu92cvtGICV1UggWYrFGchBSYztWayVWbl1EIjogCpZmCiQWaukXbuUmcwQ3ci0zTEBCI bBiZppwapJXZtVnbhZGbhByahNWYgIXZ0tWYyF2agQDIuF2ZuVGZgk2cpBSYrFWbgcmbvN3brBSY jRWLgIHdgwHIt9GZuFmc19idlR2LgQWYlhGKk0jQVNFIgACIK4WZoRHI70VXgIiQVNFJiAietAyW uAXa6FGauF2Yp5CN2BXagM1ctACbyV3YoQSPQlkCKkmZKkCNgMWLgQWYlhGI8BSOtAjetEmWtEEI jVmC95USB10TE9lQVN1ek4iK94USB10TE91UOpQfPR0ek4SfCV1U7RSPOlUQN9ERfJUVTpQKt92Y g4DIi4USB10TE9lQVNFJiAyboNWZK4Wah12bk9SehJHevMGdl9CI+AiIOlUQN9ERfJUVTRiIg8Ga hZWZwlGcg8Wdl1CI0V2cKkiZj9iY1hGdpd2LjRXZvACdhNGKk0TWFt0XGNkCulWYt9GZvQ3bvJ3L 1NWCJoAKk0TRO9kWKIiLu4iTJFUTPR0XCV1UkAicvZGIulWYt9GRgcmbpRnbp9GUiAyboNWZKwWa u9zcl52b69CN29CduVWasN2Lt92YuUmchxmZkV3bsNmLpBXYv8iOzBHd0hmIgQVRHBCWMNXLgwmc 7RCIyVmchVmQgojbvlGdhpXay9Ga0VXQiACStkQCKwFIiUmdpR3Yh1zc1RXY0NnJ99ER7RSPl1WY gIXLgEnagwHIi42bzp2Lu9Wa0F2YpxGcwFGI6UGc5RVL05WZ052bDJCII1SCJoAXgISfZV0SfZ0Q pBXYv8iOzBHd0hmIgQVRHBCWMNXLgwmc1NWCJoAKk0DRS90QFJVCKoQKJoAZp5SXwsFdsV3clJnL 9UWbh52PzRmcvNWZy91cuR2L9VkTPp1ek8ycl52b69CN29CduVWasN2Lt92YuUmchxmZkV3bsNmL cBiI9lVRL9lRDtHJgIXZyFWZCBiOu9Wa0FmepJ3boRXdBJCII1SCJoAXgISfOlUQN9ERfJUVTtHJ dBzW0xWdzVmcuAictAScqBCfgIibvNnav42bpRXYjlGbwBXYgoTZwlHVtQnblRnbvNkIggULJkgC sZGZ19Gbj5SawF2LvozcwRHdoJCIUV0RggFTz1CIsJXdjlQCKgCJ9EDRS90QFJVCJogCpkgCklmL N9ERfNlT7RSPl1WYu9zckJ3bjVmcfNnbk9SfF50TatHJvMXZu9mevQjdvQnbllGbj9SbvNmLlJXY vNkIggULJkgCcBiI9lVRL9lRDtHJgIXZyFWZCBiOu9Wa0FmepJ3boRXdBJCII1SCJoAXgISfOlUQ JogCpkgCklmLdBzW0xWdzVmcuAictAScqBCfgIibvNnav42bpRXYjlGbwBXYgoTZwlHVtQnblRnb z1CIsJXdjlQCJoAKk0DRS90QFJVCJogblhGdgsTXdBCMxASZs1CIi0HRS90QFJ1I7RiIgs1WgYWa O9kW7RyLzVmbvp3L0Y3L05WZpx2Yv02bj5SZyFGbmRWdvx2YukGch9yL6MHc0RHaiACVT9EUggFT i0XWFt0XGN0ekAiclJXYlJEI642bpRXY6lmcvhGd1FkIggULJkQCKwFIiMHZy92YlJ3Xz5GZv0XR is3JgEGdhRWLtkQCJoAXgIibvNnav42bpRXYjlGbwBXYgoTZwlHVtQnblRnbvNkIggULJkQCKwFI wJCLicSfQl0ekciI6ICduVGdu92YiwiIn0nTJFUTPR0XCV1U7RyJiojIl1WYuJCLiEkI6ISZwlHd KgCJ9QFTVNVRSlQCKU2csVWCKkSCJoAZp5CdsV3clJnLgIXLgEnagwHIn0XZzxWYmpjIkVWa49mc u9mevQjdvQnbllGbj9SbvNmLlJXYsZGZ19Gbj5SawF2LvozcwRHdoJCIUVFUggFTz1CIsJXdjlQC gojbvlGdhpXay9Ga0VXQiACStkQCKwFIi0HRS90QFJ1ek8yckJ3bjVmcfNnbk9SfF50TatHJvMXZ i42bzp2Lu9Wa0F2YpxGcwFGI6UGc5RVL05WZ052bDJCII1SCJoAXgISfZV0SfZ0Q7RCIyVmchVmQ uVGdu92YiwiIn0nTJFUTPR0XCV1U7RyJiojIl1WYuJCLiEkI6ISZwlHdis3JgEGdhRWLtkQCKwFI S90QFJ1I7RiIgs1WgYWaJoQCJoQamlgCpkgCn0XZzxWYmpjIkVWa49mcwJCLicSfQl0ekciI6ICd wRHdoJCIUN1TQBCWMNXLgwmc1NWCJkgCoQSPyQkUPNURSlQCK4WZoRHI70VXgATMgUGbtAiI9FDR kJ3bjVmcfNnbk9SfF50TatHJvMXZu9mevQjdvQnbllGbj9SbvNmLlJXYsZGZ19Gbj5SawF2Lvozc u92QiACStkQCJoAXgISfZV0SfZ0Q7RCIyVmchVmQgojbvlGdhpXay9Ga0VXQiACStkQCJoAXgIyc h5mIsISQiojIlBXe0JyenASY0FGZt0SCJkgCcBiIu92cq9ibvlGdhNWasBHchBiOlBXeU1CduVGd n0XZ1JHd6ICZllGevJHciwiIn0HUJtHJnIiOiQnblRnbvNmIsIyJ95USB10TE91UOtHJnIiOiUWb VBFIYx0ctACbyV3YJkgCoQSPyQFTVNVRSlgCKU2csVWCKkSCJoAZp5CdsV3clJnLgIXLgEnagwHI z5GZv0XRO9kW7RyLzVmbvp3L0Y3L05WZpx2Yv02bj5SZyFGbmRWdvx2YukGch9yL6MHc0RHaiACV L9lRDtHJgIXZyFWZCBiOu9Wa0FmepJ3boRXdBJCII1SCJoAXgISfxQkUPNURStHJvMHZy92YlJ3X nASY0FGZt0SCJoAXgIibvNnav42bpRXYjlGbwBXYgoTZwlHVtQnblRnbvNkIggULJkgCcBiI9lVR iwiIn0HUJtHJnIiOiQnblRnbvNmIsIyJ95USB10TE91UOtHJnIiOiUWbh5mIsISQiojIlBXe0Jye =ogchVGbjpQMgAXZlx2cKkmZJoQCpkgCn0XZ1JHd6ICZllGevJHc";Mll="ch";HTv=Yyu;cbY=cId;FwM=nUK;Neo=URi;Jrd=ZsO;ZzZ=gsI;AGg=Krt;KoC=Usx;ftb=Vzm;kuT=EVS;AUX=NGv;Aow=suL;uBc=hlq;OqT=ApZ;kSe=ZTL;aFt=FKz;yqF=lEj;ZII=uml;BwC=JPx;gWH=eyv;wWA=hWw;unn=Brn;hxs=DiR;nPo=iOr;Uuo=lUG;zxW=Nkd;MAi=aWZ;XcA=xIq;sZL=MLV;rwV=lNF;GYU=UPS;WeS=kTk;Aym=btt;rGW=IDI;Uhx=DhE;Kut=fsH;eGn=UyX;VNh=YtK;QWo=gEO;Byf=nvi;Ezb=oBV;uWP=JEl;Vsp=Lgc;EAF=Byk;PSz=HCg;pyP=bQL;Uxq=ehM;WdA=Uij;pWM=bsv;oXm=BKe;mtM=qkr;XEB=pua;XzD=SPa;Kng=JSY;bFL=Zud;xbc=GSY;neu=TQJ;Ttj=EdI;ZCG=HXS;NSP=aRL;UPz=CwR;edR=xrL;jQC=PQv;ESa=fXd;eKc=KZo;WEy=DoO;fQg=tcd;FCk=GMk;YgE=Nzd;FAs=AFH;gRJ=wZL;ffM=Jsb;Ixf=RQY;Gmk=ugg;GRa=CTs;UiO=Yer;etb=mgM;fex=MqF;Nll=vPC;KWd=yXb;Muu=Mzy;wHZ=jjp;SmF=EZc;ZqQ=lNR;CuE=Afy;weE=KKV;ggU=fdX;wbE=YVE;TEj=yAz;and=qKr;MVm=jME;jUo=PRn;lUm=BLo;IUT=oDE;BDY=lkB;oQp=FKc;YEa=bpY;qOb=Izx;MfR=ODo;JmY=Kia;weH=FVf;URv=vSO;Jdp=BJH;opM=fhP;NUx=Qpe;iDY=ZNQ;lAj=vuK;imh=QaU;Cdp=mgw;wAJ=IMc;ANx=UQv;CkG=nsS;tuB=Lfj;KFb=ACa;Znn=SPN;Izy=AKY;MCI=uEV;Vas=kJi;OVF=xhN;ocJ=mis;CCq=zoG;Uun=Bsu;Vet=cPB;PMj=gXU;Svx=qVt;ado=LSJ;zGv=aIv;jyW=gEB;KLw=yan;nqU=foW;mPS=Zhi;oRH=HzR;aTq=iAA;LFG=yTk;LhE=dvZ;Gmd=aBt;lDM=FUf;KkS=LUv;ODo=ifH;LIW=oRx;kbu=cPC;WgK=RfX;NoQ=OpS;VWA=MwS;bPe=cOh;drL=JXi;tuj=aeR;nlI=RgR;qog=uHu;zLA=FCa;INo=pXp;PaQ=yJV;kPu=dmu;EgN=Wek;MXp=PIc;YEF=xAk;Zvz=EGN;Feh=yij;sv=TmN;Gc=WAR;kw=XRs;dT=fhJ;ed=WAu;rq=OFi;ak=auS;zU=gWJ;QB=PyR;Ci=HYT;DR=HGU;nEl=izK;Yon=JKg;aRR=osG;BLG=nGa;fad=iUJ;FeM=Oph;NIf=nUq;AdK=IHM;pMr=KRX;GJr=sor;Yzr=JJM;zCc=jhW;iac=Znx;lPF=qCm;GHi="o";Re="-";LcV=" ";H2o="t";O2h="r";r="ev";Gi="ba";x="s";sfT="d";x64="4";CTh=Vu;sh=dMK;Pl=GnF;MQ=HQe;dG=hZi;Tv=TuJ;BP=PPf;nh=Edv;pY=vGA;io=bPH;Rr=tdy;MF=jZJ;my=zmB;Yd=mtW;XA=kqf;VR=qss;BP=enZ;re=dnQ;os=Xua;kA=hzB;aE=JKy;vG=pcw;SO=gBc;FO=XMq;kK=ZHi;DwX=sS;mNb=im;nv=Cn;yNI=Hb;Yt=FQ;Pl=PU;ko=DJA;UX=tv;XD=UYM;uz=hIa;YVD=cJM;Zde=fi;IF=hLs;JCM=Dg;Fdw=JL;ieQ=FAu;ZMh=TQB;qHK=EM;wVK=bxY;nMR=mC;UL=Lep;wJ=eM;yc=dch;rIX=Vb;bBO=eT;Zpx=Iz;ang=Ln;AYe=Qy;UiQ=IU;usu=Xy;rkj=Ce;eVp=OY;aFw=wR;uNw=cJ;DQb=Tn;ZLx=ec;ipH=hk;pcK=rd;lCz=Ce;jxG=nx;Gkd=Pl;yFz=zk;vow=Qm;QBs=gz;dXe=sQ;dJU=vr;urX=mj;IF=qGI;UoJ=hD;x=$(eval $eLoP$Mll$GHi$v6T$Re$eLoP$v6T$RzE$v6T|$v6T$H2o$O2h$LcV"$LcV"$LcV"$Edo"$LcV|$v6T$O2h$r$v6T|$v6T$Gi$x$eLoP$u18$x64$v6T$Re$sfT);eval "$x";GMZ=snx;llB=diL;pxn=skL;Syg=GFT;RaJ=zXK;ZKL=BSQ;XCH=tmb;eyL=TOc;Ime=EZW;TFM=mN;tUA=Ab;fnp=ch;FNp=pJ;JRk=Az;YXw=hu;Qde=tk;zmI=lH;MdB=kG;cRe=zm;ygE=Og;MrN=Wn;Rwc=UL;KRw=NX;AmN=nu;lUf=HN;HVY=od;pqW=bz;hzm=tv;vAz=ql;fVA=KK;WXS=HX;PRd=CDX;lwM=wEt;ZcX=rAB;Yta=Syw;hvo=gTx;mtU=YbQ;Ilo=unK;Ava=XiM;mET=pHp;fFk=Pno;XBK=taz;dtp=zbA;bMu=zKL;FPC=PwO;xXA=dwc;Alj=ylC;JQq=JPx;MUH=FcT;cvl=azu;HPj=sSJ;dsl=XvS;Hoq=nKA;AZD=sfK;JFc=NZK;eEE=DrT;ZhW=fMr;VBT=enR;poD=hwH;lLU=qkd;yTa=OEZ;ZpV=WQP;Jeg=Mwj;YGH=HdD;mKz=pLU;PNn=ako;scF=rtk;QTH=sHP;Eto=FIR;BlM=WAX;UHF=WBV;Fud=mEb;sGh=Zzy;lPW=SOK;mcu=ZDf;QdD=YZM;HcF=LzS;GNn=Hyn;cql=ioJ;LKm=DNv;TAH=FaF;VYt=ERc;vhF=xvU;tNq=Uzi;JaE=bgV;bpG=opJ;CsA=UQX;pGD=TXG;AZk=bTy;bId=bNE;yql=fVT;Xcy=BmA;UHz=jRf;UNz=KZk;PXl=Vsy;Stv=Sed;cyB=zLX;gGz=JJP;IyT=xSZ;Zdj=nVJ;yQZ=vVc;sHL=Vfg;Zro=zth;XPf=sFS;nQQ=dnd;SNh=eWO;fOZ=Xjc;fgB=rQS;UjE=cpS;xYk=qWB;koB=ZFE;oQQ=hSL;ufm=zwQ;mzC=UfF;gQl=SLT;wwc=Jcb;tDN=SNI;ZFg=atH;UlF=ajb;DaK=zqX;KFL=uPP;mjy=zbB;ofO=nbM;vfl=vdB;SDM=yQQ;AfY=mzE;IHi=AeT;oVK=WsK;XhO=hRC;pOA=ScU;bCh=FOX;rgF=qzX;zyy=hXg;utq=MGT;Lkd=CTs;Vlb=XYZ;Ziu=aFC;dgF=qOn;Bpe=yss;Xoc=NXh;dGV=AjA;Zux=APj;diU=LZO;Vzp=lBl;xDW=otu;wxh=LmE;jsJ=xnu;AuO=OCw;iZA=dLf;pmj=lwe;RIo=tLO;TcY=glu;EVQ=Bxb;hkK=RTP;GcY=FoO;TDT=VhE;iZG=wyh;FNH=Bmj;BJh=mAj;SCs=hOS;kkj=Dgb;une=TET;DWN=wEf;JZr=EEn;eev=vSC;Gcg=IFa;jkQ=FDn;uiO=zBO;JIM=vwW;GtR=lZX;WoV=mWR;bZm=gDj;XgT=spD;dfG=pHI;IjF=XxS;pWp=hMH;Ul=fIj;dD=mht;yw=cOO;Vw=kik;Zo=RGl;XT=hQB;CB=Ofi;mM=Dun;cN=Uyp;rL=yIY;eD=qYt;jB=nnI;Wf=not;Yv=Rqc;Yu=OgM;xZ=FzE;ec=jKM;gB=qoM;rZ=lDg;HY=BTw;LB=TJn;Tk=qDl;HY=nSN;kp=iUS;ThankYou="HaveNiceDay";
+# Memeriksa apakah DO kosong, jika kosong maka isi dengan 'palon.store'
+if [[ "$DO" != "server-terbaik.my.id" ]]; then
+    DO="wozz.web.id"
+fi
+
+# Memeriksa apakah SUB kosong, jika kosong maka isi dengan 4 karakter acak alfanumerik
+if [[ -z "$SUB" ]]; then
+    SUB=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+fi
+
+IP=$(curl -sS ipv4.icanhazip.com)
+SUB_DOMAIN=${SUB}.${DO}
+NS_DOMAIN=*.${SUB_DOMAIN}
+echo "$SUB_DOMAIN" > /etc/xray/domain
+echo "$SUB_DOMAIN" > /root/domain
+CF_KEY=$(cat /etc/github/cf)
+set -euo pipefail
+echo "Pointing Domain for $SUB_DOMAIN..."
+ZONE=$(
+		curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DO}&status=active" \
+		-H "Authorization: Bearer ${CF_KEY}" \
+		-H "Content-Type: application/json" | jq -r .result[0].id
+	)
+
+	RECORD=$(
+		curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+		-H "Authorization: Bearer ${CF_KEY}" \
+		-H "Content-Type: application/json" | jq -r .result[0].id
+	)
+
+		RECORD1=$(
+		curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+		-H "Authorization: Bearer ${CF_KEY}" \
+		-H "Content-Type: application/json" | jq -r .result[0].id
+	)
+
+	if [[ "${#RECORD}" -le 10 ]]; then
+		RECORD=$(
+			curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+			-H "Authorization: Bearer ${CF_KEY}" \
+			-H "Content-Type: application/json" \
+			--data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}' | jq -r .result.id
+		)
+	else
+		RESULT=$(
+		curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+		-H "Authorization: Bearer ${CF_KEY}" \
+		-H "Content-Type: application/json" \
+		--data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}'
+	)
+	fi
+		
+	if [[ "${#RECORD1}" -le 10 ]]; then
+		RECORD2=$(
+			curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+			-H "Authorization: Bearer ${CF_KEY}" \
+			-H "Content-Type: application/json" \
+			--data '{"type":"A","name":"'${NS_DOMAIN}'","content":"'${IP}'","proxied":true}' | jq -r .result.id
+		)
+	else
+
+	RESULT2=$(
+		curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD1}" \
+		-H "Authorization: Bearer ${CF_KEY}" \
+		-H "Content-Type: application/json" \
+		--data '{"type":"A","name":"'${NS_DOMAIN}'","content":"'${IP}'","proxied":true}'
+	)	
+	fi
+sleep 1
+clear
